3c47b77293ba92a5d8f9cb9dadf5c620afb78b2d
Sergey Voronin <Sergey.Voronin@axpo.com>
2022-11-23 23:41:11 +0100

    fixing bugs

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -4,20 +4,20 @@ function cachingDecoratorNew(func) {
   function wrapper(...args) {
     const hash = args.join(","); // получаем правильный хэш
     let objectInCache = cache.find((item) => item.hash == hash); // ищем элемент, хэш которого равен нашему хэшу
+    if (objectInCache) {
+      // если элемент найден
 
-    if (!objectInCache) {
-      // если элемент не найден
-      let result = func(...args); // в кэше результата нет - придётся считать
-      cache.push({ hash: hash, value: result }); // добавляем элемент с правильной структурой
-      if (cache.length > 5) {
-        cache.shift(); // если слишком много элементов в кэше надо удалить самый старый (первый)
-      }
-      console.log("Вычисляем: " + result);
-      return "Вычисляем: " + result;
+      console.log("Из кэша: " + objectInCache.value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+      return "Из кэша: " + objectInCache.value;
     }
 
-    console.log("Из кэша: " + cache.find((el) => el.hash == hash).value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
-    return "Из кэша: " + cache.find((el) => el.hash == hash).value;
+    let result = func(...args); // в кэше результата нет - придётся считать
+    cache.push({ hash: hash, value: result }); // добавляем элемент с правильной структурой
+    if (cache.length > 5) {
+      cache.shift(); // если слишком много элементов в кэше надо удалить самый старый (первый)
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
   }
   return wrapper;
 }
@@ -28,22 +28,19 @@ function debounceDecoratorNew(func, delay) {
   wrapper.allCount = 0;
   function wrapper(...args) {
     if (wrapper.allCount == 0) {
-      delay_adj = 0;
-    } else {
-      delay_adj = delay;
+      func(...args);
+      wrapper.count += 1;
     }
     wrapper.allCount += 1;
 
     if (timeoutId) {
-      debugger;
       clearTimeout(timeoutId);
     }
 
     timeoutId = setTimeout(() => {
       func(...args);
-
       wrapper.count += 1;
-    }, delay_adj);
+    }, delay);
   }
   return wrapper;
 }
